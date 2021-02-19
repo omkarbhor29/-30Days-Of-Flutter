2162bb81a2df09245f1328d8164c50e9eb3c8b9e
omkarbhor29 <omkarbhor2908@gmail.com>
2021-02-19 14:10:45 +0530

    Day 14 completed Gridview

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -40,13 +40,47 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-              ? ListView.builder(
-                  itemCount: CatalogModel.items.length,
+              // ? ListView.builder(
+              //     itemCount: CatalogModel.items.length,
+              //     itemBuilder: (context, index) {
+              //       return ItemWidget(
+              //         item: CatalogModel.items[index],
+              //       );
+              //     },
+              //   )
+              ? GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10.0, //horizpntai space
+                      mainAxisSpacing: 16.0),
                   itemBuilder: (context, index) {
-                    return ItemWidget(
-                      item: CatalogModel.items[index],
-                    );
+                    final item = CatalogModel.items[index];
+                    return Card(
+                        clipBehavior: Clip.antiAlias,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        child: GridTile(
+                          header: Container(
+                            padding: EdgeInsets.all(12),
+                            child: Center(
+                                child: Text(
+                              item.name,
+                              style: TextStyle(color: Colors.white),
+                            )),
+                            decoration: BoxDecoration(color: Colors.deepPurple),
+                          ),
+                          child: Image.network(item.image),
+                          footer: Container(
+                              padding: EdgeInsets.all(12),
+                              decoration: BoxDecoration(color: Colors.black),
+                              child: Center(
+                                  child: Text(
+                                item.price.toString(),
+                                style: TextStyle(color: Colors.white),
+                              ))),
+                        ));
                   },
+                  itemCount: CatalogModel.items.length,
                 )
               : Center(
                   child: CircularProgressIndicator(),
